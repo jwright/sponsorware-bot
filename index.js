f7cfe734bbc6d7a3e9a2054cc436ced3bf678147
Jamie Wright <jamie@brilliantfantastic.com>
2020-08-26 22:32:41 -0400

    Refactor to extract the setup to a module

--- index.js
+++ index.js
@@ -1,15 +1,5 @@
-const express = require('express')
-const path = require('path')
+const postInstallSetup = require('./lib/postInstallSetup')
 
 module.exports = robot => {
-  const app = express()
-  app.set('view engine', 'hbs')
-  app.set('views', path.join(__dirname, 'views'))
-
-  app.get('/post-install', (req, res) => {
-    const webhookPath = `${req.protocol}://${req.get('host')}${process.env.WEBHOOK_PATH}`
-    res.render('instructions', { webhook_path: webhookPath, webhook_secret: process.env.WEBHOOK_SECRET })
-  })
-
-  robot.router.use(app)
+  postInstallSetup(robot.router)
 }

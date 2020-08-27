ebbffdc738d834afe5242345c08da8602c5b47bf
Jamie Wright <jamie@brilliantfantastic.com>
2020-08-26 20:10:28 -0400

    Display the webhook secret so it can be used in the post install instructions

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
-/**
- * This is the main entrypoint to your Probot app
- * @param {import('probot').Application} app
- */
-module.exports = app => {
-  // Your code here
-  app.log('Yay, the app was loaded!')
+const express = require('express')
+const path = require('path')
 
-  app.on('issues.opened', async context => {
-    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
-    return context.github.issues.createComment(issueComment)
+module.exports = robot => {
+  robot.log('Yay, the app was loaded!')
+
+  const app = express()
+  app.set('view engine', 'hbs')
+  app.set('views', path.join(__dirname, 'views'))
+
+  app.get('/post-install', (req, res) => {
+    res.render('instructions', { webhook_secret: process.env.WEBHOOK_SECRET })
   })
 
-  // For more information on building apps:
-  // https://probot.github.io/docs/
-
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
+  robot.router.use(app)
 }
